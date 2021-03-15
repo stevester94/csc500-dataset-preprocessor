4def2efa496c6f2133412b9481eb00e666f0d53e
Steven Mackey <stevester94@gmail.com>
2021-03-14 22:13:30 -0700

    We are now writing and reading TFRecords

---
+++ record_generator.py
@@ -0,0 +1,74 @@
+#! /usr/bin/python3
+import tensorflow as tf
+
+####################
+# Below are ripped from https://www.tensorflow.org/tutorials/load_data/tfrecord
+####################
+
+def _bytes_feature(value):
+    """Returns a bytes_list from a string / byte."""
+    if isinstance(value, type(tf.constant(0))):
+        value = value.numpy() # BytesList won't unpack a string from an EagerTensor.
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+def _float_feature(value):
+    """Returns a float_list from a float / double."""
+    return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+
+def _int64_feature(value):
+    """Returns an int64_list from a bool / enum / int / uint."""
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+####################
+# End ripped portion
+####################
+
+# Create a dictionary with features that may be relevant.
+def build_ofdm_frame_example(device_id_int64, day_int64, ofdm_symbol_bytes, time_domain_IQ_int64):
+
+    feature = {
+        'device_id': _int64_feature(device_id_int64),
+        'day': _int64_feature(day_int64),
+        'ofdm_symbol': _bytes_feature(ofdm_symbol_bytes),
+        'time_domain_IQ': _bytes_feature(time_domain_IQ_int64),
+    }
+
+    return tf.train.Example(features=tf.train.Features(feature=feature))
+
+
+# I have found that instantiating this guy on every function call adds non-trivial overhead,
+# so declare him outside of the function
+_ofdm_example_description = {
+    'device_id'     : tf.io.FixedLenFeature([], tf.int64, default_value=0),
+    'time_domain_IQ': tf.io.FixedLenFeature([], tf.string, default_value=''),
+    'ofdm_symbol'   : tf.io.FixedLenFeature([], tf.string, default_value=''),
+    'day'           : tf.io.FixedLenFeature([], tf.int64, default_value=0),
+}
+def parse_serialized_ofdm_frame_example(serialized_example):
+    return tf.io.parse_single_example(serialized_example, _ofdm_example_description)
+
+def write_examples_to_records(examples, record_path):
+    with tf.io.TFRecordWriter(record_path) as writer:
+        for example in examples:
+            writer.write(example.SerializeToString())
+
+
+if __name__ == "__main__":
+    ex1 = build_ofdm_frame_example(1, 1, bytes("123", 'utf-8'), bytes("5678", 'utf-8'))
+    ex2 = build_ofdm_frame_example(2, 2, bytes("1337", 'utf-8'), bytes("42", 'utf-8'))
+
+    examples = [ex1, ex2]
+
+    write_examples_to_records(examples, "testing.tfrecords")
+
+    # Now read it back
+    raw_dataset = tf.data.TFRecordDataset("testing.tfrecords")
+    parsed_dataset = raw_dataset.map(parse_serialized_ofdm_frame_example)
+
+    print(parsed_dataset)
+
+    for features in parsed_dataset:
+        print(features)
+
+
+    # Keras expects either an X and Y dataset for the model, or a single dataset which yields (x, y)
