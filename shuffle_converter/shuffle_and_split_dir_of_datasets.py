1d2c47dc45b524e5d86ccc9279ad49f84d6467b4
Steven Mackey <stevester94@gmail.com>
2021-05-01 20:58:22 -0700

    Make test of randomizer more robust

--- shuffle_converter/shuffle_and_split_dir_of_datasets.py
+++ shuffle_converter/shuffle_and_split_dir_of_datasets.py
@@ -37,21 +37,24 @@ def binary_random_chunk_generator(seed, paths, chunk_length, disable_randomizati
         
         yield buf
 
-def test(ds_path):
+def test(ds_path_1, ds_path_2):
     symbol_size = 384
     record_size = symbol_size + 1 + 1 + 1 + 8
 
-    ds = symbol_dataset_from_file(ds_path, batch_size=1).as_numpy_iterator()
+    ds = symbol_dataset_from_file(ds_path_1, batch_size=1)
+    ds = ds.concatenate(symbol_dataset_from_file(ds_path_2, batch_size=1))
+
+    ds_iter = ds.as_numpy_iterator()
     gen = binary_random_chunk_generator(
         1337,
-        [ds_path],
+        [ds_path_1, ds_path_2],
         record_size,
         disable_randomization=True
     )
 
 
     print("Getting cardinality...")
-    ds_cardinality = get_iterator_cardinality(ds)
+    ds_cardinality = get_iterator_cardinality(ds_iter)
     gen_cardinality = get_iterator_cardinality(gen)
 
     print("ds_cardinality:", ds_cardinality)
@@ -60,16 +63,16 @@ def test(ds_path):
     assert(  ds_cardinality == gen_cardinality )
 
     # Rebuild our iterators since we exhausted them in the cardinality test
-    ds = symbol_dataset_from_file(ds_path, batch_size=1).as_numpy_iterator()
+    ds_iter = ds.as_numpy_iterator()
     gen = binary_random_chunk_generator(
         1337,
-        [ds_path],
+        [ds_path_1, ds_path_2],
         record_size,
         disable_randomization=True
     )
 
     print("Comparing all items")
-    for orig, new in zip(ds, gen):
+    for orig, new in zip(ds_iter, gen):
         sym = symbol_tuple_from_bytes(new)
 
         assert( np.array_equal( orig[0], sym[0] ) )
@@ -78,6 +81,41 @@ def test(ds_path):
         assert( np.array_equal( orig[3], sym[3] ) )
         assert( np.array_equal( orig[4], sym[4] ) )
 
+
+    ds_iter = ds.as_numpy_iterator()
+    gen = binary_random_chunk_generator(
+        1337,
+        [ds_path_1, ds_path_2],
+        record_size,
+        disable_randomization=False
+    )
+    print("Getting cardinality of randomized set")
+    ds_cardinality = get_iterator_cardinality(ds_iter)
+    gen_cardinality = get_iterator_cardinality(gen)
+    print("ds_cardinality:", ds_cardinality)
+    print("gen_cardinality:", gen_cardinality)
+    assert(  ds_cardinality == gen_cardinality )
+
+    ds_iter = ds.as_numpy_iterator()
+    gen = binary_random_chunk_generator(
+        1337,
+        [ds_path_1, ds_path_2],
+        record_size,
+        disable_randomization=False
+    )
+    test_pass = False
+    print("Comparing all items again (this time randomized, so should 'fail' quickly)")
+
+    # If we are truely randomizing this will fail quickly
+    for orig, new in zip(ds_iter, gen):
+        sym = symbol_tuple_from_bytes(new)
+
+        if not np.array_equal( orig[0], sym[0] ):
+            test_pass = True
+            break
+    if not test_pass:
+        raise Exception("Generators were equivalent when they should not be")
+
     print("Test Passed")
     sys.exit(0)
 
@@ -85,17 +123,17 @@ def test(ds_path):
 
 def print_usage():
     print("Usage: <in dir of datasets> <out dir of shuffled and split datasets>")
-    print("       test <path of shuffled dataset file>")
+    print("       test <path of shuffled dataset file> <path of another shuffled dataset file>")
 
 if __name__ == "__main__":
 
 
-    if len(sys.argv) != 3:
+    if len(sys.argv) != 4 and len(sys.argv) != 3:
         print_usage()
         sys.exit(1)
 
     if sys.argv[1] == "test":
-        test(sys.argv[2])
+        test(sys.argv[2], sys.argv[3])
         sys.exit(0)
 
     in_dir, out_dir = sys.argv[1:]
