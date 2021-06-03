aa7f478ed1d42eeee6601754e4d2f619d4f25d02
Steven Mackey <stevester94@gmail.com>
2021-06-02 20:07:08 -0700

    Distance 4 is jacked up

--- build_windowed_datasets_stride-20_oneDistance.bash
+++ build_windowed_datasets_stride-20_oneDistance.bash
@@ -27,7 +27,8 @@ output_window_size="128"
         # ],
 
 #for distance in 2 4 8 14 20 26 32 38 44 50 56 62; do
-for distance in 8; do
+# for distance in 2 4 32 38 50 56; do
+for distance in 4; do
     (
         working_dir="/mnt/wd500GB/CSC500/csc500-super-repo/datasets/automated_windower/windowed_EachDevice-200k_batch-100_stride-20_distances-$distance/"
         mkdir -p $working_dir &&
