b99dcfd31db0c25ff53107362a2221441c5d5683
Steven Mackey <stevester94@gmail.com>
2022-01-29 16:04:49 -0800

    Beginnings of packet indices

---
+++ Analysis/packet_indices.m
@@ -0,0 +1,62 @@
+f = fopen("/mnt/wd500GB/CSC500/csc500-super-repo/datasets/KRI-16Devices-RawData/14ft/WiFi_air_X310_3123D52_14ft_run1.sigmf-data",'r');
+% f = fopen("/mnt/wd500GB/CSC500/csc500-super-repo/csc500-dataset-preprocessor/Analysis/cores_one.bin");
+% f = fopen("5_fake_packets.bin");
+rx = fread(f, 'double');
+fclose(f);
+
+
+% rx = [zeros(4000,1); rx]; % Append zeros
+% rx = [zeros(4000,1); rx(1:500000)]; % Take subset
+% rx = [rx(1:500000)]; % Take subset
+
+i = rx(1:2:end);
+q = rx(2:2:end);
+rx = complex(i,q);
+
+
+% Get matlab's detection decisions on a per index basis
+offset = 0;
+threshold = 1.0;
+bandwidth=5e6;
+[startOffset,M] = wlanPacketDetect(rx,"CBW20", offset,threshold);
+display(startOffset);
+% plot(M)
+xlabel('Samples')
+ylabel('Decision Statistics')
+
+indices = [];
+offset = 1;
+max_search=20000;
+while 1
+    start = find(M(offset:end) > 0.99, 1);
+    
+    if isempty(start)
+        display("No start found");
+        break;
+    end
+    
+    start = start(1)+offset-1;
+    offset = start;
+    finish = find(M(offset:end) < 0.5, 1);
+
+    if isempty(finish)
+        display("No finish found");
+        break;
+    end
+
+    finish = finish(1)+offset-1;
+    offset = finish;
+    
+    indices(end+1) = start;
+    display(start);
+end
+
+
+% while 1
+%     
+% 
+% end
+
+
+
+return;
